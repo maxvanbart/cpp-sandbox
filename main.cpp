983f047984da117bc546650ec8366b7e0c4266bc
maxvanbart <maxvanbart@gmail.com>
2023-02-17 17:25:36 +0100

    initial commit

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int fibonacci(int n) {
+    if (n == 1) {
+        return 1;
+    } else if (n < 1) {
+        return 0;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+
+int main() {
+    std::cout << "Hello world!\n";
+    int i = 8;
+
+    std::cout << i << "th fibonacci number: " << fibonacci(i) << "\n";
+
+
+    return 0;
+}
