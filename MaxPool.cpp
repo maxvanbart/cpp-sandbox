a9f30bab3769db5a81d3127689c0ecfb90fb43e4
Jenny <jen@theapples.eu>
2023-03-23 15:27:43 +0100

    Maxpool

---
+++ MaxPool.cpp
@@ -0,0 +1,16 @@
+//
+// Created by jen on 23/03/2023.
+//
+
+#include "MaxPool.h"
+
+# Pad the input
+int x_padded = pad(x, [self.padding] * 4)
+
+# Unpack the needed dimensions
+N, C, H, W = x.shape
+KS = self.kernel_size
+
+# Calculate output height and width
+Hp = 1 + (H + 2 * self.padding - KS) // self.stride
+Wp = 1 + (W + 2 * self.padding - KS) // self.stride
---
+++ MaxPool.h
@@ -0,0 +1,8 @@
+//
+// Created by jen on 23/03/2023.
+//
+
+#ifndef SANDBOX_MAXPOOL_H
+#define SANDBOX_MAXPOOL_H
+
+#endif //SANDBOX_MAXPOOL_H
